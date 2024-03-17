bc9c65fafa58f47fac098d34216c13c5ddabbda8
Thomas Ulrich <thomasulrich1402@gmail.com>
2024-03-17 14:19:20 -0400

    Added all completed assignments

---
+++ UlrichThomasExercise1.js.js
@@ -0,0 +1,76 @@
+const albums = [
+    {
+        id: 2125,
+        title: 'Use Your Illusion',
+        artist: "Guns N' Roses"
+    },
+
+    {
+        id: 1678,
+        title: 'A Night at the Opera',
+        artist: 'Queen'
+    },
+
+    {
+        id: 2975,
+        title: '1999',
+        artist: 'Prince'
+    },
+
+    {
+        id: 3257,
+        title: 'Space Oddity',
+        artist: 'DavidBowie'
+    }
+
+];
+
+//return an album that matches the id
+function findById(id) {
+    return albums.find(album => album.id === id);
+}
+
+//To do: implement save(album)
+function save(album) {
+    albums.push(album);
+}
+
+//To do: implement findByArtist(artist)
+function findByArtist(artist) {
+    return albums.filter(album => album.artist === artist);
+}
+
+
+//To do: implement updateById(id, album)
+function updateById(id, album){
+    let al = findById(id);
+    if (al === undefined) return false;
+    al = album;
+    return true;
+}
+
+//To do: implement removeById(id)
+function removeById(id){
+    const index = albums.indexOf(findById(id));
+    if (index === -1) return false;
+    albums.splice(index, 1);
+    return true;
+}
+
+//To do: uncomment the following testing code when you are ready to test your functions
+
+save({ id: 1458, title: "G N' R Lies", artist: "Guns N' Roses" });
+save({ id: 3590, title: "School's Out", artist: 'Alice Cooper' });
+save({ id: 1257, title: 'Transformer', artist: 'Lou Reed' });
+console.log(albums);
+console.log(findById(2975));
+console.log(findByArtist("Guns N' Roses"));
+console.log(removeById(4000));
+console.log(updateById(4000, {}));
+console.log(updateById(3257, {
+    id: 3257,
+    title: 'Space Oddity',
+    artist: 'David Bowie'
+}));
+console.log(removeById(1678));
+console.log(albums);
